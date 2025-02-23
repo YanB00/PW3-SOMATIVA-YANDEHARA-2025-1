336bac75520f21534c6b2e6e20d4e1267aa5422e
Suporte NC7 <ybarbosa648@gmail.com>
2025-02-22 23:58:26 -0300

    Estilização do BookCard e criação e Estilização do Button

--- pw3-somativa-2025.1/src/components/BookCard.jsx
+++ pw3-somativa-2025.1/src/components/BookCard.jsx
@@ -1,11 +1,14 @@
-const BookCard = ({title, author, image})=>{
+import style from './BookCard.module.css'
+import Button from './Button'
+
+const BookCard = ({titulo, autor, imagem}) => {
     return(
-        <div>
-            <h3>{title}</h3>
-            <p>{author}</p>
-            <img src={image} alt='Capa:Fundação'/>
+        <div className={style.bookCard}>
+            <h3 className={style.title}>{titulo}</h3>
+            <p className={style.author}>{autor}</p>
+            <img src={imagem} alt="Capa: A fundação"/>
+            <Button label= 'DETALHE' />
         </div>
     )
 }
-
 export default BookCard
---
+++ pw3-somativa-2025.1/src/components/Button.jsx
@@ -0,0 +1,13 @@
+import style from './Button.module.css'
+
+const  Button = ({label}) => {
+
+    return(
+        <div className={style.buttonContainer}>
+        <button>{label}</button>
+    </div>
+
+    )
+}
+
+export default Button

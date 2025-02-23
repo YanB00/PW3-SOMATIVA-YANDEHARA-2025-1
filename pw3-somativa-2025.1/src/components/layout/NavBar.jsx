f86c5b30926fd8c05dfee213e91e556376b70512
Suporte NC7 <ybarbosa648@gmail.com>
2025-02-23 00:04:27 -0300

    Inicio da barra de navegação

---
+++ pw3-somativa-2025.1/src/components/layout/NavBar.jsx
@@ -0,0 +1,14 @@
+import style from './NavBar.module.css'
+
+const NaveBar = () =>{
+    return (
+        <nav>
+            <ul>
+                <li>HOME</li>
+                <li>CADASTRAR LIVROS</li>
+                <li>LISTAR LIVROS</li>
+            </ul>
+        </nav>
+    )}
+
+export default NaveBar

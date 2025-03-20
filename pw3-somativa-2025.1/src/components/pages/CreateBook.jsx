fbfd3a4abaed107c728966fb3845393b08552fbb
Suporte NC7 <ybarbosa648@gmail.com>
2025-03-20 13:01:49 -0300

    Implementação do recebemento dos dados de cadastro de livro com uso de states no REACT.

--- pw3-somativa-2025.1/src/components/pages/CreateBook.jsx
+++ pw3-somativa-2025.1/src/components/pages/CreateBook.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import style from './CreateBook.module.css'
 
 import Input from "../form/Input";
@@ -6,18 +7,37 @@ import Select from "../form/Select";
 import Button from "../form/Button";
 
 const CreateBook = ()=>{
+
+    const [book, setBook] = useState({})
+
+    function handlerChangeBook(event){
+        setBook({... book, [event.target.name] :event.target.value });
+        console.log(book);
+    }
+
+    function handlerChangeCategory(event){
+        setBook({... book, category: event.target.options[event.target.selectedIndex].text})
+    }
+
+    function submit(event){
+        event.preventDefault();
+        console.log(book);
+    }
+
     return(
 
         <section className={style.create_book_container}>
 
             <h1>CADASTRO DE LIVRO</h1>
 
+            <form onSubmit={submit}>
             <Input
                 text='Nome do livro'
                 type='text'
                 name='txt_livro'
                 id='txt_livro'
                 placeholder='Digite o nome do livro'
+                handlerChange={handlerChangeBook}
             />
 
             <Input
@@ -26,6 +46,7 @@ const CreateBook = ()=>{
                 name='txt_autor'
                 id='txt_autor'
                 placeholder='Digite o nome do autor do livro'
+                handlerChange={handlerChangeBook}
             />
 
             <Input
@@ -34,17 +55,20 @@ const CreateBook = ()=>{
                 name='txt_descricao'
                 id='txt_descricao'
                 placeholder='Digite a descrição do livro'
+                handlerChange={handlerChangeBook}
             />
 
             <Select 
                 name='slc_categoria'
                 id='slc_categoria'
                 text='Categoria do livro'
+                handlerChange={handlerChangeCategory}
             />
 
             <Button
                 label='CADASTRAR LIVRO'
             />
+            </form>
 
         </section>
 
